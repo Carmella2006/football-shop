3b821a2fb11c8525da9b3f25fb96140c13482b25
Carmella2006 <carmella.gerldine@ui.ac.id>
2025-09-07 23:29:43 +0700

    refresh pls

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'footballshop.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_shop.settings')
->>>>>>> origin/main
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
