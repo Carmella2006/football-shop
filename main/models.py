736c3eddfe25ea0910228a422b17ea665fdbd04f
Carmella2006 <carmella.gerldine@ui.ac.id>
2025-09-13 22:36:57 +0700

    ganti category

--- main/models.py
+++ main/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('shoes', 'Shoes'),
+        ('jersey', 'Jersey'),
+        ('ball', 'Ball'),
+        ('accessories', 'Accessories'),
+    ]
+
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='shoes')
     is_featured = models.BooleanField(default=False)
     stock = models.IntegerField(default=0)
     brand = models.CharField(max_length=100, blank=True)
